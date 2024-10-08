4af027783c61638608e14119efaa49d1431a0b28
WonGi-Kim <kwg970310@gmail.com>
2024-10-08 13:52:53 +0900

    first commit

---
+++ src/main/java/com/example/onemonth/OneMonthApplication.java
@@ -0,0 +1,13 @@
+package com.example.onemonth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OneMonthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OneMonthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/onemonth/OneMonthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onemonth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OneMonthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
